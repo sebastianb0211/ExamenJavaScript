988dfa7099f94a686a5e725a1654fcabaf528979
Sebastianb <sebastianb.0211@gmail.com>
2023-10-10 10:43:17 -0500

    se crea plantilla del formulario

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function poner() {
+    console.log("hola");
+    let botonPrimero = document.getElementsByClassName("quitar-clases")[0];
+    let botonSegundo = document.getElementsByClassName("agregar-clases")[0];
+  
+    botonSegundo.style.display = "none";
+    botonPrimero.style.display = "flex";
+    botonPrimero.style.alignItems = "center";
+  }
+  
+  function quitar() {
+    let botonPrimero = document.getElementsByClassName("quitar-clases")[0];
+    let botonSegundo = document.getElementsByClassName("agregar-clases")[0];
+  
+    botonPrimero.style.display = "none";
+    botonSegundo.style.display = "flex";
+    botonSegundo.style.alignItems = "center";
+  
+  }
+  
