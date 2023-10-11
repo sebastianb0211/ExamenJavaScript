5842d9e691536126fdff2d924a08d2f73e1d9a79
Sebastianb <sebastianb.0211@gmail.com>
2023-10-10 22:08:10 -0500

    se crea proyecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,34 @@ function poner() {
     console.log("hola");
     let botonPrimero = document.getElementsByClassName("quitar-clases")[0];
     let botonSegundo = document.getElementsByClassName("agregar-clases")[0];
+    let container = document.getElementById('containerUno')
+    let imagen = document.getElementById('container-imagenUno')
+    let formulario = document.getElementById('form-containerUno')
   
     botonSegundo.style.display = "none";
     botonPrimero.style.display = "flex";
     botonPrimero.style.alignItems = "center";
+    container.style.display='flex';
+    container.style.justifyContent = 'center';
+    imagen.style.display = "flex";
+    formulario.style.display='none';
+    
   }
   
   function quitar() {
     let botonPrimero = document.getElementsByClassName("quitar-clases")[0];
     let botonSegundo = document.getElementsByClassName("agregar-clases")[0];
+    let container = document.getElementById('containerUno')
+    let imagen = document.getElementById('container-imagenUno')
+    let formulario = document.getElementById('form-containerUno')
   
     botonPrimero.style.display = "none";
     botonSegundo.style.display = "flex";
     botonSegundo.style.alignItems = "center";
+    container.style.display='none';
+    imagen.style.display = "none";
+    formulario.style.display='flex';
+    
   
   }
 
